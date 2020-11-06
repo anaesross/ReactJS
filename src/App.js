625ec893ff2853256e358c482ba647a21af902a9
Anaê Soares <anae08@gmail.com>
2020-11-06 17:35:46 -0300

    projeto finalizado

--- src/App.js
+++ src/App.js
@@ -1,15 +1,119 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+
 import Comentario from './components/Comentario'
 
-
-function App() {
+//criar função
+/*function App() {
     return ( 
-    <div className = "App">
-       <h1>Meu Projeto</h1>
-        <Comentario />
-    </div>
+        <div className = "App">
+            <h1>Meu Projeto</h1>
+            <Comentario nome="Anaê" email="anae@email.com" data={new Date(2020, 11, 5)}>
+                Olá, tudo bem?
+            </Comentario>
+            <Comentario nome="Maria" email="maria@email.com" data={new Date(2020, 11, 5)}>
+                Olá, tudo bem? 
+            </Comentario>
+        </div>
     );
-}
+}*/
+//Criar classe
+class App extends Component { //component é do próprio react
+    //hooks
+    state = { //state é um objeto, o 'estado' da aplicação
+        comentarios: [ //componentes que possuem states , propriedades são chamados de stateful components(componentes inteligentes)
+            {
+                nome:'Anaê', 
+                email:"anae@email.com", 
+                data:new Date(2020, 11 , 5, 17, 30 ,0), 
+                mensagem:'Olá, tudo bem?'
+            },
+            {
+                nome:'Maria', 
+                email:"maria@email.com", 
+                data:new Date(2020, 11 , 5, 9, 15, 0), 
+                mensagem:'Olá, tudo bem?'
+            }
+        ],
+        novoComentario:{
+            nome: '',
+            email: '',
+            mensagem:''
+        }
+    }
+    adicionarComentario = (evento) =>{
+        evento.preventDefault(); //barra o post
+        console.log("Adicionando comentário");
 
+        /*const novoComentario = {
+            nome:'Anaê', 
+            email:"anae@email.com", 
+            data:new Date(2020, 11 , 5), 
+            mensagem:'Olá, tudo bem?'
+        }*/
+        //set state função, herdade de components react
+        //let lista = this.state.comentarios;
+        //lista.push(novoComentario);
+        //this.setState( {comentarios: lista} );
+        const novoComentario = { ...this.state.novoComentario, data:new Date() } //spread pega o que ja tem no elemento e adiciona a data
+        this.setState({
+            comentarios: [...this.state.comentarios, novoComentario], //utilizando o spread, adicionando um novo elemento no array comentarios
+            novoComentario: { nome: ' ', email: '' , mensagem:''} //limpar os campos do formulário após envio
+        })
+    }
+    removerComentario = comentario => {
+        let lista = this.state.comentarios;
+        lista = lista.filter(c => c !== comentario) //remover lista filtrando todos os comentarios cujo seja diferente do comentario que estou passando na function
+        this.setState({ comentarios: lista })
+    }
+    digitacao = evento =>{
+        //console.log(evento.target.value);
+        const {name, value} = evento.target; //criando duas variáveis
+        //const value = evento.target.value
+        //const name = evento.target.name;
+        this.setState({ novoComentario: {...this.state.novoComentario, [name]: value} }) //mudar o state do componente, pegar o que ja tem e adicionar novos valores que serão digitados no form
+    }
+    render () { 
+        return (
+        <div className = "App">
+            <h1>Meu Projeto</h1>
+            {this.state.comentarios.map((comentario, indice) =>(//map percorre uma lista de array e retorna o elemento - fazendo loop
+                //console.log(comentario)
+            <Comentario 
+                key={indice} //cada filho da lista tem que ter uma propriedade única - podemos criar um id no objeto e passar comentario.id também ou utilizar somente o indice mesmo
+                nome={comentario.nome} 
+                email={comentario.email} 
+                data={comentario.data}
+                onRemove={this.removerComentario.bind(this, comentario)}> 
+                {comentario.mensagem}
+            </Comentario>
+            //bind() = O método bind() cria uma nova função que, quando chamada, tem sua palavra-chave this definida com o valor fornecido
+            //onRemove = passando a referencia da função para o componente comentario.js para remover o comentario
+            /*<Comentario nome="Maria" email="maria@email.com" data={new Date(2020, 11, 5)}>
+                Olá, tudo bem? 
+            </Comentario>*/
+            ))}
+            <form method="post" onSubmit={this.adicionarComentario} className="Novo-Comentario">
+                <h2>Adicionar Comentário</h2>
+                <div>
+                    <input required
+                        type="text" 
+                        name="nome" 
+                        value={this.state.novoComentario.nome} 
+                        onChange={this.digitacao} 
+                        placeholder="Digite seu nome"
+                    />
+                </div>
+                <div>
+                    <input required type="email" name="email" value={this.state.novoComentario.email} onChange={this.digitacao}  placeholder="Digite seu email"/>
+                </div>
+                <div>
+                    <textarea required name="mensagem" rows="4" value={this.state.novoComentario.mensagem} onChange={this.digitacao} ></textarea>
+                </div>
+                <button type="submit">Adicionar Comentário</button>
+            </form>
+        </div>
+        );
+    }
+}
 export default App;
--- src/components/Comentario.js
+++ src/components/Comentario.js
@@ -1,14 +1,37 @@
 import React from 'react';
+import { formatRelative } from 'date-fns'
+import { ptBR} from 'date-fns/locale'
+
 import './Comentario.css'
+import imagemUsuario from './user.png'
 
 //um componente é uma function, um componente pode rertornar apenas um único elemento no caso 1 <div>
 //criando um componente, sempre começa com letra maiúscula
 //criar um elemento que englobe todos os outros vazia ou qlqr elemento ex <> </>
-const Comentario = () => (
-    <div className="Comentario">
+/*const Comentario = () => {
+    const num1 = 4;
+    const num2 = 5;
+   return <div className="Comentario">
         <div>Anaê:</div>
         <div>Olá, tudo bem? Eu sou um componente</div>
+        <div>2 + 3 = {num1 + num2} </div> 
     </div> //html dentro do javascript = jsx
-); //retorna apenas o codigo do componente, utilizar () e não {}
-
+}; *///retorna apenas o codigo do componente, utilizar () e não {}= usar quando retornar elementos 
+const Comentario = (props) => { //passando parâmetro props = objeto que vem com as propriedades do nosso elemento //Propriedades nao podem ser alteradas, são apenas read-only
+    const estilo = { //criando elementos CSS no javascript
+        color:'red',
+        padding:'10px',
+        fontSize: '30px'
+    }
+    return <div className="Comentario">
+        <img class="avatar" src={imagemUsuario} alt={props.nome}/>
+        <div class="conteudo">
+            <h2 style={estilo}>{props.nome}</h2>
+            <p class="email">{props.email}</p>
+            <p class="mensagem">{props.children}</p> 
+            <p class="data">{formatRelative(props.data, new Date(), {locale: ptBR})}</p>
+            <button onClick={props.onRemove}>&times;</button>
+        </div>
+    </div> //html dentro do javascript = jsx //a msg é um elemento filho
+}; //componentes que só possuem propriedades chamam de stateless components (componentes sem estado - componentes burros)
 export default Comentario;
